006f02d7391c0d0903760c003b9c3db505537faf
irinadeeva <deeva97@me.com>
2024-12-10 15:30:28 +0300

    done

--- ForegroundNextTrack/ContentView.swift
+++ ForegroundNextTrack/ContentView.swift
@@ -7,18 +7,63 @@
 
 import SwiftUI
 
+struct NextTrackButtonStyle: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .frame(width: 100)
+      .padding()
+      .scaleEffect(configuration.isPressed ? 0.86 : 1.0)
+      .animation(
+        .easeInOut(duration: 0.22),
+        value: configuration.isPressed
+      )
+      .background(Color.blue.opacity(configuration.isPressed ? 0.3 : 0))
+      .clipShape(
+        Circle()
+      )
+
+  }
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State var isNextTrack: Bool = false
+
+  var body: some View {
+    Button {
+      withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) {
+        isNextTrack.toggle()
+      } completion: {
+        isNextTrack.toggle()
+      }
+    } label: {
+      GeometryReader { proxy in
+        let size = proxy.size.width / 2
+
+        HStack(spacing: -1) {
+          buttonImage
+            .frame(width: isNextTrack ? size : 0)
+            .opacity(isNextTrack ? 1 : 0)
+
+          buttonImage
+            .frame(width: size)
+
+          buttonImage
+            .frame(width: isNextTrack ? 1 : size)
+            .opacity(isNextTrack ? 0 : 1)
         }
-        .padding()
+        .frame(maxHeight: .infinity, alignment: .center)
+      }
     }
+    .buttonStyle(NextTrackButtonStyle())
+  }
+
+  var buttonImage: some View {
+    Image(systemName: "play.fill")
+      .resizable()
+      .scaledToFit()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
