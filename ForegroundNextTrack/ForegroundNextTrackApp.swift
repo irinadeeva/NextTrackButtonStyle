31e2e9f8973d0971531965f21f41f2814256b035
irinadeeva <deeva97@me.com>
2024-12-10 14:10:08 +0300

    Initial Commit

---
+++ ForegroundNextTrack/ForegroundNextTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForegroundNextTrackApp.swift
+//  ForegroundNextTrack
+//
+//  Created by Irina Deeva on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ForegroundNextTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
